c4edb2ee5550d658a8bd663360949811253950cd
Dennis Ploetner <re@lloc.de>
2024-05-14 17:43:35 +0200

    Composer update -W --no-dev added

--- src/Commands/PluginComposerUpdateCommand.php
+++ src/Commands/PluginComposerUpdateCommand.php
@@ -13,7 +13,7 @@ class PluginComposerUpdateCommand extends PluginCommand implements CommandInterf
 		$output  = shell_exec( $command );
 
 		if ( ! empty( $output ) ) {
-			push_webhook_logger()->error( 'Git pull failed', array( 'output' => $output ) );
+			push_webhook_logger()->error( 'composer update -W --no-dev failed', array( 'output' => $output ) );
 
 			return false;
 		}
